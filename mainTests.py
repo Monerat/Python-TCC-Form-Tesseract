a09d611b32fcaf90db497ad3dc079c151dc86458
Monerat <gmonerat@gmail.com>
2022-04-24 12:31:01 -0300

    Função main testes

---
+++ mainTests.py
@@ -0,0 +1,7 @@
+from FuncTestes import testePrecisaoAlgoritmo
+
+
+formulariosParaTestes = ["form1","form2","form3","form4","form5"]
+
+for i in formulariosParaTestes:
+    testePrecisaoAlgoritmo(i)
